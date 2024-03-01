94fac81c5360fb228412fca5df2065d9c6d343c8
AbiskarLc <lcabi116@gmail.com>
2024-03-01 18:39:06 +0545

    improved and added

--- api/Database/connection.js
+++ api/Database/connection.js
@@ -1,18 +1,13 @@
 const mongoose = require('mongoose');
 const url =  process.env.DATABASE_URL;
 const connectDbs = async () =>{
-    try {
       await mongoose.connect(url).then(()=>{
 
         console.log("Connected to Database Successfully")
-      }).catch(()=>{
-        console.log("Connection Failed")
+      }).catch((err)=>{
+        console.log(err);
       });
-     
-        
-    } catch (error) {
-        
-    }
+ 
         
 
 };
